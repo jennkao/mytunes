ab7c6ca8e1c008c5d2d4356e0836f83e9fc1b232
Vinh Vu <vu.vinh.van@gmail.com>
2016-11-14 19:34:05 -0800

    passed Songs spec

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,18 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+  initialize: function() {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs',
+      method: 'GET',
+      // dataType: 'application/json',
+      // contentType: 'application/json',
+      success: function(data) {
+        console.log(data.results);
+        this.add(data.results);
+      }.bind(this)
+    })
+  }
 
 });
