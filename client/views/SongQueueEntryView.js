eb81192cb37a59c288290198bf7f528717fe61e4
jennkao <jennkao92@gmail.com>
2016-11-15 11:38:17 -0800

    pass songQueue spec

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -3,15 +3,7 @@ var SongQueueEntryView = Backbone.View.extend({
   // your code here!
   tagName: 'tr',
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
-  initialize: function() { 
-  },
-  events: function() {
-
-  },
-  handleClick: function() {
-
-  },
   render: function() {
-
+    return this.$el.html(this.template(this.model.attributes));
   }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,20 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add', function() {
+      this.render();
+    }, this);
+    this.collection.on('pop', function() {
+      this.render();
+    }, this);
   },
-
   render: function() {
-    return this.$el;
+    this.$el.empty();
+    this.collection.forEach(this.renderSongQueueEntry, this);
+  },
+  renderSongQueueEntry: function(song) {
+    var songQueueEntryView = new SongQueueEntryView({model: song});
+    this.$el.append(songQueueEntryView.render());
   }
 
 });
