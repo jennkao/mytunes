c89b1924eadec98538f6ecac475f98343520ab1e
jennkao <jennkao92@gmail.com>
2016-11-15 17:38:22 -0800

    complete collections

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,29 +1,25 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
-
   model: SongModel,
-
   initialize: function() {
-    this.on('add', function() {
+    this.on('add', function(song) {
       if (this.length === 1) {
         this.playFirst();
       }
     }, this);
     this.on('ended', function(song) {
-      console.log(song);
       this.remove(this.at(0));
       if (this.length > 0) {
         this.playFirst();
       }
     }, this);
     this.on('dequeue', function(song) {
-      this.remove(song.cid);
+      this.remove(this.at(0));
     }, this);
   },
   playFirst: function() {
     this.at(0).play();
   }
-
 });
 
 
--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,19 +1,15 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
-
   model: SongModel,
+  url: 'https://api.parse.com/1/classes/songs',
   initialize: function() {
-    // $.ajax({
-    //   url: 'https://api.parse.com/1/classes/songs',
-    //   method: 'GET',
-    //   // dataType: 'application/json',
-    //   // contentType: 'application/json',
-    //   success: function(data) {
-    //     console.log(data.results);
-    //     this.add(data.results);
-    //   }.bind(this)
-    // });
-    
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs',
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+      success: function(data) {
+        this.add(data.results);
+      }.bind(this)
+    }); 
   }
-
 });
