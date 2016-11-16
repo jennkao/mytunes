53e9f5488bdabd198563ed792ce2f5b89f1aa7bb
jennkao <jennkao92@gmail.com>
2016-11-15 17:37:28 -0800

    complete song queue view with entries

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,6 +1,5 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
-
   initialize: function() {
     this.collection.on('add', function() {
       this.render();
@@ -8,14 +7,17 @@ var SongQueueView = Backbone.View.extend({
     this.collection.on('pop', function() {
       this.render();
     }, this);
+    this.collection.on('ended', function() {
+      this.render();
+    }, this);
   },
   render: function() {
     this.$el.empty();
-    this.collection.forEach(this.renderSongQueueEntry, this);
-  },
-  renderSongQueueEntry: function(song) {
-    var songQueueEntryView = new SongQueueEntryView({model: song});
-    this.$el.append(songQueueEntryView.render());
-  }
 
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
 });
